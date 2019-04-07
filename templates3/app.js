51535e626cccc345ea664960130e2efc1249b9f8
Moe Quinn <moequinn50@gmail.com>
2019-04-07 00:24:37 -0400

    add post, publish js

---
+++ templates3/app.js
@@ -0,0 +1,23 @@
+/* write post section */
+const submitButton = document.getElementById('submit-post');
+const writePost = document.getElementById('write-post');
+submitButton.onclick = function(event) {
+	console.log(writePost.value);
+};
+
+
+/* sign up */
+const signupButton = document.getElementById('submit-sign-up');
+const signupUsername = document.getElementById("sign-up-username");
+const signupEmail = document.getElementById("sign-up-email");
+const signupPassword = document.getElementById("sign-up-password");
+const signupBorough = document.getElementById('borough');
+signupButton.onclick = function() {
+	const username = signupUsername.value;
+	const email = signupEmail.value;
+	const password = signupPassword.value;
+	const b = signupBorough.value;
+	
+	const response = document.getElementById('signup-response');
+	response.textContent = "Hello " + username + ", from " + b + ". Welcome to My App."
+};
---
+++ templates3/js/posts.js
@@ -0,0 +1,40 @@
+window.addEventListener('load', function() {
+    // post code
+});
+
+const postsDiv = document.getElementById('posts');
+const postRef = firebase.database().ref('posts');
+postRef.on('child_added', function(snapshot) {
+    createPost(snapshot.val());
+});
+
+function createPost(post) {
+    const postDiv = document.createElement('div');
+    postDiv.classList.add('post');
+    const postText = document.createElement('div');
+    postText.classList.add('post-text');
+    const postInfo = document.createElement('div');
+    postInfo.classList.add('post-info');
+    
+    const postAuthor = document.createElement('span');
+    postAuthor.classList.add('post-author');
+    const postDate = document.createElement('span');
+    postDate.classList.add('post-date');
+    
+    postText.textContent = post.text;
+    postAuthor.textContent = post.author;
+    postDate.textContent = new Date(post.date).toLocaleDateString('en-us'
+        , { month: 'long', year: 'numeric', weekday: 'long', day: 'numeric'})
+    
+    postDiv.appendChild(postText);
+    postDiv.appendChild(postInfo);
+    postInfo.innerHTML += " by ";
+    postInfo.appendChild(postAuthor);
+    postInfo.innerHTML += " on ";
+    postInfo.appendChild(postDate);
+    
+    /* chronological order */
+    // postsDiv.appendChild(postDiv);
+    /* reverse chronological order */
+    postsDiv.insertBefore(postDiv, postsDiv.firstElementChild);
+}
